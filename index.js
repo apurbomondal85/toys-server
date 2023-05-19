ade50cbe9aba99452d2f3cd998a914698597ecd9
Apurbo Mondal <apurbo.simt.2020@gmail.com>
2023-05-19 21:05:15 +0600

    get by username is complete

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -27,17 +27,53 @@ async function run() {
 
         const toysCollection = client.db("toysDB").collection("toys");
 
+        const indexKeys = {name: 1};
+        const indexOptions = {name: "name"};
+
+        const result = await toysCollection.createIndex(indexKeys, indexOptions)
+
+
+
         app.get('/toys', async(req, res) =>{
             const result = await toysCollection.find().toArray();
             res.send(result);
         })
 
-        app.get('/toys/:category', async(req, res) => {
+        app.get('/toys/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)};
+            const result = await toysCollection.findOne(query);
+            res.send(result)
+        })
+
+        app.get('/toys/category/:category', async(req, res) => {
             const category = req.params.category;
             if (category === "All") {
                 return;
             }
             const query = {"category": category}
+            const result = await toysCollection.find(query).toArray();
+            res.send(result)
+        })
+
+        app.get('/limitToys', async(req, res)=>{
+            const limit = parseInt(req.query.limit);
+            const result = await toysCollection.find().limit(limit).toArray();
+            res.send(result)
+        })
+
+        app.get('/searchByName/:name', async(req, res) => {
+            const searchValue = req.params.name;
+            if (!searchValue) {
+                return res.status(400).json({ error: 'Search term is required' });
+              }
+            const result = await toysCollection.find({ name: { $regex: searchValue, $options: "i" } }).toArray();
+            res.send(result)
+        })
+
+        app.get('/username', async(req, res) => {
+            const username = req.query.email;
+            const query = {"email": username};
             const result = await toysCollection.find(query).toArray();
             res.send(result)
         })
